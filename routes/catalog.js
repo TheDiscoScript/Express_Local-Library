dfda12f19a3161628403e1f04ac7b24b134a766f
Willdooo <wollmada@gmail.com>
2021-09-18 17:27:23 +0200

    Create logic done

--- routes/catalog.js
+++ routes/catalog.js
@@ -15,7 +15,7 @@ router.get("/", book_controller.index); //This actually maps to /catalog/ becaus
 // GET request for creating a Book. NOTE This must come before routes that display Book (uses id).
 router.get("/book/create", book_controller.book_create_get);
 
-// POST request for creating Book.
+// POST request for creating Book. - validation
 router.post("/book/create", book_controller.book_create_post);
 
 // GET request to delete Book.
